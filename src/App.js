72777eeb43e5b12864aaa9bdf338b6986f36bcba
Jessica Tirado <jessicasoto7@gmail.com>
2018-10-31 19:15:50 -0500

    Working on rendering Places API and markers

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div>
+          <h1> Parks in Minneapolis, MN</h1>
+        </div>
         <Map />
       </div>
     );
--- src/components/map.js
+++ src/components/map.js
@@ -1,48 +1,83 @@
 import React, { Component } from 'react';
-import {withGoogleMap, GoogleMap, } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap, } from 'react-google-maps';
 import  PlaceMarker  from './marker.js'
 
-const GoogleMaps = withGoogleMap(props => (
+const google = window.google;
+
+const GoogleMaps = withScriptjs(withGoogleMap(props => (
   <GoogleMap
-    defaultCenter = {props.center}
-    defaultZoom = {props.zoom}
-  >
-  {props.places}
+    onTilesLoaded={props.fetchPlaces}
+    ref={props.onMapMounted}
+    onBoundsChanged={props.fetchPlaces}
+    defaultCenter={props.center}
+    defaultZoom={props.zoom} >
+
+    {props.places && props.places.map((place, i) =>
+        <PlaceMarker
+          key={i}
+          position={{
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng()
+          }} />
+    )}
   </GoogleMap>
-));
+)));
 
 class Map extends Component {
-  constructor(props) {
-    super(props)
 
-    this.zoom = 13
+  componentWillMount() {
+    const refs = {}
 
-    this.state = {
-      lat: 44.986656,
-      lng: -93.258133
-    };
-  }
+      this.setState({
+        bounds: null,
+        center: {
+          lat: 44.986656,
+          lng: -93.258133
+        },
+        places:[],
+        onMapMounted: ref => {
+          refs.map = ref;
+        },
+        fetchPlaces:() => {
+           const bounds = refs.map.getBounds();
+           const service = new google.maps.places.PlacesService(refs.map.context.MAP);
+           const request = {
+               bounds: bounds,
+               type: ['park']
+           };
+           service.nearbySearch(request, (results, status) => {
+             if (status === google.maps.places.PlacesServiceStatus.OK) {
+               console.log(results);
+             }
+           })
+         }
+       })
+    }
+
   render(){
-    const {lat, lng} = this.state;
-    const places = [<PlaceMarker lat={lat} lng={lng} name={"Park"} description={"Park desc"}/>]
 
-  return(
-    <div style={{ height: '100vh',
-      width: '100%',
-      display: 'flex',
-      flexFlow: 'row nowrap',
-      justifyContent: 'center',
-      padding: 0 }}>
+      return(
 
-        <GoogleMaps
-          center = {{ lat: lat, lng: lng}}
-          zoom = {this.zoom}
-          places={places}
-          containerElement={ <div style={{ width: '100%', marginLeft: 0 }} /> }
-          mapElement={ <div style={{ height: `100%` }} /> }
-        />
-      </div>
-    );
-  }
+        <div style={{ height: '100vh',
+          width: '100%',
+          display: 'flex',
+          flexFlow: 'row nowrap',
+          justifyContent: 'center',
+          padding: 0 }}>
+
+            <GoogleMaps
+              role="application"
+              aria-label="map"
+
+              zoom={13}
+
+              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDQx0zrGHKCqC5PpgVCNP8dQCBB5v_-VFM&libraries=places"
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={ <div style={{ width: '100%', marginLeft: 0 }} /> }
+              mapElement={ <div style={{ height: `100%` }} /> }
+            />
+          </div>
+        );
+    }
 };
 export default Map;
