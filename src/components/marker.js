dcb0987925e134a2db240ae547f62eb9d1d093e3
Jessica Tirado <jessicasoto7@gmail.com>
2018-11-01 13:56:52 -0500

    Working on map rendering

--- src/components/marker.js
+++ src/components/marker.js
@@ -1,42 +1,14 @@
 import React, { Component } from 'react'
-import { Marker } from 'react-google-maps'
-import  ParkInfoWindow  from './InfoWindow.js'
+//import { Marker } from 'react-google-maps'
+//import  ParkInfoWindow  from './InfoWindow.js'
 
 class PlaceMarker extends Component {
-  constructor(props) {
-    super(props)
-
-     this.state = {
-      showTooltip: false
-    }
-  }
-
-  clickTooltip() {
-    this.setState({ showTooltip: !this.state.showTooltip })
-  }
-
-  closeWindow() {
-    this.setState({ showTooltip: false })
-  }
 
   render() {
-    const {showTooltip} = this.state
-    const {lat, lng, name, description} = this.props
-
     return(
-      <Marker
-        position={{
-          lat: parseFloat(lat),
-          lng: parseFloat(lng)
-        }}
-        onClick={this.clickTooltip.bind(this)}>
-        {showTooltip && (
-          <ParkInfoWindow
-            description={description}
-            name={name}
-            closeWindow={this.closeWindow.bind(this)}/>
-        )}
-      </Marker>
+      <div>
+        PlaceMarker
+      </div>
     );
   }
 }
