21e48c2921179f6ec44cc2f4a84dbac2d339a4b7
Jessica Tirado <jessicasoto7@gmail.com>
2018-11-04 18:57:55 -0600

    Added foursuare API and is now a setState.

--- src/App.js
+++ src/App.js
@@ -4,27 +4,15 @@ import Map from './components/map.js'
 import  PlaceMarker  from './components/marker.js'
 
 class App extends Component {
+
+
   render() {
-    const location = {
-      lat: 44.986656,
-      lng: -93.258133
-    }
-
-    const markers = [
-      {
-        location: {
-          lat: 44.986656,
-          lng: -93.258133
-        }
-      }
-    ]
-
     return (
       <div className="App">
         <div>
           <h1> Parks in Minneapolis, MN</h1>
         </div>
-        <Map center={location} markers={markers}/>
+        <Map />
         <PlaceMarker />
       </div>
     );
--- src/components/map.js
+++ src/components/map.js
@@ -1,46 +1,76 @@
 import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap, InfoWindow} from 'react-google-maps';
 //import  PlaceMarker  from './marker.js'
+import axios from 'axios'
 
 //const google = window.google;
 
 const GoogleMaps = withScriptjs(withGoogleMap(props => (
   <GoogleMap
-    defaultCenter={props.center}
-    defaultZoom={props.zoom} >
-
+    defaultCenter={{lat: 44.986656, lng: -93.258133}}
+    defaultZoom={13} >
   </GoogleMap>
 )));
 
 class Map extends Component {
-  constructor(props) {
-    super(props)
-
-    this.zoom = 13
-
-    this.state = {
-      lat: 44.986656,
-      lng: -93.258133
-    }
-  }
+  state = {
+    places:[],
+    map: null,
+    markers:[],
+    markerProps:[],
+    activeMarker: null,
+    activeMarkerProps: null,
+    showingInfoWindow: false,
+  };
 
   componentDidMount() {
-    fetch('https://api.foursquare.com/v2/venues/explore?near=Minneapolis&client_id=OXBDHJTAQEW2GZ2HILCTVWROC0PSQYPZPQGVQURTO5HRUGXY&client_secret=H5ZL5RVF12D5VAZCW2PUEUFE3UCZLTH044MJCO2V1PQZHK5A&v=20180323&limit=1&5=40.7243,-74.0018&query=outdoors',
-      {
-        method:"GET"
-      }
-    )
-    .then(function() {
-        // Code for handling API response
-    })
-    .catch(function() {
-        // Code for handling errors
-    });
+    const url = 'https://api.foursquare.com/v2/venues/explore?'
+    const parameters = {
+      client_id: 'OXBDHJTAQEW2GZ2HILCTVWROC0PSQYPZPQGVQURTO5HRUGXY',
+      client_secret:'H5ZL5RVF12D5VAZCW2PUEUFE3UCZLTH044MJCO2V1PQZHK5A',
+      query:'outdoors',
+      near:'Minneapolis',
+      v:'20180323',
+      limit: '5'
+    }
+
+    axios.get(url + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({
+          places: response.data.response.groups[0].items
+        })
+      })
+      .catch(error => {
+          console.log('ERROR!' + error)
+      });
   }
+
+  mapReady = (props, map) => {
+    this.SetState({map});
+  }
+
+  closeWindow = () => {
+    this.state.activeMarker && this
+      .state
+      .activeMarker
+      .setAnimation(null);
+    this.SetState({showingInfoWindow: false, activeMarker: null, activeMarkerProps: null});
+
+  }
+
+  onMarkerClick = (props, marker, e) => {
+    this.closeInfoWindow();
+
+    this.setState({showingInfoWindow:true, activeMarker: marker, activeMarkerProps:props});
+  }
+
 
   render(){
 
-      const {lat, lng} = this.state;
+      const center = {
+        lat: this.props.lat,
+        lng: this.props.lng
+      }
 
       return(
 
@@ -55,18 +85,17 @@ class Map extends Component {
               role="application"
               aria-label="map"
 
-              center = {{
-                lat: lat,
-                lng: lng
-              }}
-
-              zoom={this.zoom}
+              google={this.props.google}
+              zoom = {this.props.zoom}
+              initialCenter={center}
 
               googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDQx0zrGHKCqC5PpgVCNP8dQCBB5v_-VFM&libraries=places"
               loadingElement={<div style={{ height: `100%` }} />}
               containerElement={ <div style={{ width: '100%', marginLeft: 0 }} /> }
-              mapElement={ <div style={{ height: `100%` }} /> }
-            />
+              mapElement={ <div style={{ height: `100%` }} /> } >
+
+
+            </GoogleMaps>
           </div>
         );
     }
